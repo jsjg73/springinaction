2a45a649ef0f71ece81c3a87cb10141ef081bb79
jsjg73 <kjs930703@gmail.com>
2022-07-19 19:33:59 +0900

    테스트 mockmvc 추가

--- src/test/java/tacos/TacoCloudApplicationTests.java
+++ src/test/java/tacos/TacoCloudApplicationTests.java
@@ -1,13 +1,19 @@
 package tacos;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@AutoConfigureMockMvc
 class TacoCloudApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testHomePage() throws Exception{
+
 	}
 
 }
