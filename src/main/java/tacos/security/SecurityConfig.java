b028d6c659925d53a281bd486a5ab2740de1cb15
jsjg73 <kjs930703@gmail.com>
2022-09-03 22:34:20 +0900

    jdbc 사용자 스토어 with dummy encoder

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,15 +25,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
           .httpBasic();
     }
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception { // 사용자 인증 정보 구성하는 메서드
-        auth.inMemoryAuthentication()
-                .withUser("user1")
-                .password("{noop}password1")
-                .authorities("ROLE_USER")
-                .and()
-                .withUser("user2")
-                .password("{noop}password2")
-                .authorities("ROLE_USER");
+        auth
+          .jdbcAuthentication()
+          .dataSource(dataSource)
+          .usersByUsernameQuery(
+                  "SELECT username, password, enabled FROM users WHERE username=?")
+          .authoritiesByUsernameQuery(
+                  "SELECT username, authority FROM authorities WHERE username=?")
+          .passwordEncoder(new NoEncodingPasswordEncoder());
     }
 }
