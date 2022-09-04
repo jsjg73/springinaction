dc30d939652d2de8bff7a7ce488c0b44647d8437
jsjg73 <kjs930703@gmail.com>
2022-09-04 20:50:52 +0900

    웹 페이지 인증 규칙 정의하기

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -17,13 +17,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // 사용자의 HTTP 요청 경로에 대해 접근 제한과 같은 보안 관련 처리를 우리가 원하는 대로 할 수 있게 해준다.
     @Override
     protected void configure(HttpSecurity http) throws  Exception {// 보안 정보 구성하는 메서드
+        // 사용자 등록을 위한 페이지와 인증된 사용자를 위한 페이지에 다른 접근 권한을 설정한다.
+
         http
         .authorizeRequests()
           .antMatchers("/design", "/orders")
             .access("hasRole('ROLE_USER')")
-          .antMatchers("/", "/**").access("permitAll")
-        .and()
-          .httpBasic();
+          .antMatchers("/", "/**").access("permitAll");
+
+        // 표현식을 사용할 때 장점.
+        // '화요일의 타코 생성은 ROLE_USER 권한을 갖는 사용자에게만 허용하고 싶다'는 가상의 스토리
+//        http
+//            .authorizeRequests()
+//                .antMatchers("/design", "/orders")
+//                    .access("hasRole('ROLE_USE') && T(java.util.Calendar).getInstance().get(T(java.util.Calendar).DAY_OF_WEEK) == T(java.util.Calendar).TUESDAY")
+//            .antMatchers("/", "/**").access("permtiAll");
     }
 
 //    @Autowired
