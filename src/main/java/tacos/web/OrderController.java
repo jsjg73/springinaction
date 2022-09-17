c5dc47188c5c0e63927c4f2b3b65fc1336b24eeb
jsjg73 <kjs930703@gmail.com>
2022-09-17 14:23:03 +0900

    페이징 설정에 구성 속성 적용

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,7 @@
 package tacos.web;
 import javax.validation.Valid;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -16,7 +17,20 @@ import tacos.data.OrderRepository;
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("order")
+@ConfigurationProperties(prefix = "taco.orders")
 public class OrderController {
+
+    private int pageSize = 20;
+
+    private int pageOffset = 0;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public void setPageOffset(int pageOffset) {
+        this.pageOffset = pageOffset;
+    }
 
     private final OrderRepository orderRepo;
 
@@ -68,7 +82,7 @@ public class OrderController {
 
     @GetMapping
     public String ordersForUser(@AuthenticationPrincipal User user, Model model){
-        PageRequest pageable = PageRequest.of(0, 20);
+        PageRequest pageable = PageRequest.of(pageOffset, pageSize);
         model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
         return "orderList";
     }
